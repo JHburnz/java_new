5b5c9f49cb6f7ea2d09ed48ca0e509a2605a1773
JHburnz <jiji98301@gmail.com>
2022-05-18 21:19:03 +0900

    게시물 상세페이지 기능수현, 날짜는 아직못함

--- src/com/kor/java/ssg/Main.java
+++ src/com/kor/java/ssg/Main.java
@@ -43,12 +43,37 @@ public class Main {
 					continue;
 				}
 
-				System.out.println("번호 | 제목");
+				System.out.println("번호   |   제목");
 				for (int i = articles.size() - 1; i >= 0; i--) {
 					Article article = articles.get(i);
 
-					System.out.printf("%d     | %s\n", article.id, article.title);
+					System.out.printf("%d      |   %s\n", article.id, article.title);
 				}
+			} else if (command.startsWith("article detail ")) {
+				String[] commandBits = command.split(" ");
+				int id = Integer.parseInt(commandBits[2]);
+
+				Article foundArticle = null;
+
+				for (int i = 0; i < articles.size(); i++) {
+					Article article = articles.get(i);
+
+					if (article.id == id) {
+						foundArticle = article;
+						break;
+					}
+				}
+
+				if (foundArticle == null) {
+					System.out.printf("%d번 게시물은 존재하지 않습니다.\n", id);
+					continue;
+				}
+
+				System.out.printf("번호 : %d\n", foundArticle.id);
+				System.out.printf("날짜 : 2022-05-18 19:00:00\n");
+				System.out.printf("제목 : %s\n", foundArticle.title);
+				System.out.printf("내용 : %s\n", foundArticle.body);
+
 			} else {
 				System.out.printf("%s(은)는 존재하지 않는 명령어 입니다.\n", command);
 			}
